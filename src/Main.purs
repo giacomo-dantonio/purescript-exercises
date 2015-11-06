ea2a7420380477881f10f92efc1fae85fa99f199
Giacomo d'Antonio <giacomo.dantonio@gmail.com>
2015-11-06 18:44:31 +0100

    prepare for chapter 8 (broken)

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,15 @@
 module Main where
 
+import Prelude
+
+import DOM
+
+import Control.Monad.Eff
 import Control.Monad.Eff.Console
 
+import Data.AddressBook.UI
+
+main :: forall eff. Eff (console :: CONSOLE, dom :: DOM | eff) Unit
 main = do
-  log "Hello sailor!"
+  log "Attaching event handlers"
+  setupEventHandlers 
